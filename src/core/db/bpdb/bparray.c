7ced8fedab7126c30c4cbc6d672797a834019a22
Robert Shi <robertyishi@gmail.com>
2023-09-23 20:45:54 -0700

    parallelized bparray expansion

--- src/core/db/bpdb/bparray.c
+++ src/core/db/bpdb/bparray.c
@@ -1,8 +1,8 @@
 #include "core/db/bpdb/bparray.h"
 
 #include <assert.h>    // assert
-#include <stdbool.h>   // bool
 #include <inttypes.h>  // PRIu64
+#include <stdbool.h>   // bool
 #include <stddef.h>    // NULL
 #include <stdint.h>    // int8_t, uint8_t, int32_t, int64_t, uint64_t
 #include <stdio.h>     // fprintf, stderr
@@ -41,11 +41,9 @@ static const int kDefaultBitsPerEntry = 1;
  */
 static const int kMaxBitsPerEntry = 31;
 
-static int BpArrayInitHelper(BpArray *array, int64_t size, int bits_per_entry);
-
 static int64_t GetStreamLength(int64_t num_entries, int bits_per_entry);
 
-static uint64_t GetSegment(BpArray *array, int64_t i);
+static uint64_t GetSegment(const BpArray *array, int64_t i);
 static void SetSegment(BpArray *array, int64_t i, uint64_t segment);
 static int64_t GetBitOffset(int64_t i, int bits_per_entry);
 static int64_t GetLocalBitOffset(int64_t i, int bits_per_entry);
@@ -53,14 +51,36 @@ static int64_t GetByteOffset(int64_t i, int bits_per_entry);
 static uint64_t GetEntryMask(int bits_per_entry, int local_bit_offset);
 
 static uint64_t CompressEntry(LookupDict *lookup, uint64_t entry);
-static bool ExpansionNeeded(BpArray *array, uint64_t entry);
-static int BpArrayExpand(BpArray *array, uint64_t entry);
+static bool ExpansionNeeded(const BpArray *array, uint64_t entry);
+static int BpArrayExpand(BpArray *array);
 static int ExpandHelper(BpArray *array, int new_bits_per_entry);
 
 // -----------------------------------------------------------------------------
 
 int BpArrayInit(BpArray *array, int64_t size) {
-    return BpArrayInitHelper(array, size, kDefaultBitsPerEntry);
+    array->meta.stream_length = GetStreamLength(size, kDefaultBitsPerEntry);
+    array->stream =
+        (uint8_t *)calloc(array->meta.stream_length, sizeof(uint8_t));
+    if (array->stream == NULL) {
+        fprintf(stderr, "BpArrayInit: failed to calloc array\n");
+        memset(array, 0, sizeof(*array));
+        return 1;
+    }
+
+    array->meta.bits_per_entry = kDefaultBitsPerEntry;
+    array->meta.num_entries = size;
+    int error = LookupDictInit(&array->lookup);
+    if (error != 0) {
+        fprintf(
+            stderr,
+            "BpArrayInit: failed to initialize lookup dictionary, code %d\n",
+            error);
+        free(array->stream);
+        memset(array, 0, sizeof(*array));
+        return error;
+    }
+
+    return 0;
 }
 
 void BpArrayDestroy(BpArray *array) {
@@ -82,15 +102,13 @@ uint64_t BpArrayGet(BpArray *array, int64_t i) {
 
 int BpArraySet(BpArray *array, int64_t i, uint64_t entry) {
     uint64_t compressed = CompressEntry(&array->lookup, entry);
-    if (ExpansionNeeded(array, entry)) {
-        int error = BpArrayExpand(array, compressed);
+    if (ExpansionNeeded(array, compressed)) {
+        int error = BpArrayExpand(array);
         if (error != 0) {
-            fprintf(stderr, "BpArraySet: failed to expand array, code %d\n", error);
+            fprintf(stderr, "BpArraySet: failed to expand array, code %d\n",
+                    error);
             return error;
         }
-
-        // Must compress again to register the new entry in the new dictionary.
-        compressed = CompressEntry(&array->lookup, entry);
     }
 
     int bits_per_entry = array->meta.bits_per_entry;
@@ -116,32 +134,6 @@ const int32_t *BpArrayGetDecompDict(const BpArray *array) {
 
 // -----------------------------------------------------------------------------
 
-static int BpArrayInitHelper(BpArray *array, int64_t size, int bits_per_entry) {
-    array->meta.stream_length = GetStreamLength(size, bits_per_entry);
-    array->stream =
-        (uint8_t *)calloc(array->meta.stream_length, sizeof(uint8_t));
-    if (array->stream == NULL) {
-        fprintf(stderr, "BpArrayInit: failed to calloc array\n");
-        memset(array, 0, sizeof(*array));
-        return 1;
-    }
-
-    array->meta.bits_per_entry = bits_per_entry;
-    array->meta.num_entries = size;
-    int error = LookupDictInit(&array->lookup);
-    if (error != 0) {
-        fprintf(
-            stderr,
-            "BpArrayInit: failed to initialize lookup dictionary, code %d\n",
-            error);
-        free(array->stream);
-        memset(array, 0, sizeof(*array));
-        return error;
-    }
-
-    return 0;
-}
-
 static int64_t GetStreamLength(int64_t num_entries, int bits_per_entry) {
     // 8 additional bytes are allocated so that it is always safe to read
     // 8 bytes for an entry.
@@ -149,7 +141,7 @@ static int64_t GetStreamLength(int64_t num_entries, int bits_per_entry) {
            sizeof(uint64_t);
 }
 
-static uint64_t GetSegment(BpArray *array, int64_t i) {
+static uint64_t GetSegment(const BpArray *array, int64_t i) {
     int64_t byte_offset = GetByteOffset(i, array->meta.bits_per_entry);
     const uint8_t *address = array->stream + byte_offset;
     return *((uint64_t *)address);
@@ -193,44 +185,70 @@ static uint64_t CompressEntry(LookupDict *lookup, uint64_t entry) {
     return compressed;
 }
 
-static bool ExpansionNeeded(BpArray *array, uint64_t entry) {
+static bool ExpansionNeeded(const BpArray *array, uint64_t entry) {
     return entry >= ((uint64_t)1) << array->meta.bits_per_entry;
 }
 
-static int BpArrayExpand(BpArray *array, uint64_t entry) {
+static int BpArrayExpand(BpArray *array) {
     int new_bits_per_entry = array->meta.bits_per_entry + 1;
     if (new_bits_per_entry > kMaxBitsPerEntry) {
         fprintf(stderr,
-                "BpArrayExpand: new entry %" PRIu64
-                " cannot be represented by the longest number of bits allowed "
-                "(%d) and therefore cannot be stored\n",
-                entry, kMaxBitsPerEntry);
+                "BpArrayExpand: new entry cannot be represented by the longest "
+                "number of bits allowed (%d) and therefore cannot be stored\n",
+                kMaxBitsPerEntry);
         return -1;
     }
 
     return ExpandHelper(array, new_bits_per_entry);
 }
 
-// TODO: do expansion in-place.
+static void CopyEntry(uint8_t *dest, const BpArray *src, int64_t i) {
+    int bits_per_entry = src->meta.bits_per_entry;
+    int local_bit_offset = GetLocalBitOffset(i, bits_per_entry);
+    int new_local_bit_offset = GetLocalBitOffset(i, bits_per_entry + 1);
+    uint64_t mask = GetEntryMask(bits_per_entry, local_bit_offset);
+
+    // Get segment containing the old entry.
+    uint64_t segment = GetSegment(src, i);
+    segment = (segment & mask) >> local_bit_offset;  // Get old entry.
+
+    // Make new segment.
+    int64_t byte_offset = GetByteOffset(i, bits_per_entry + 1);
+    uint8_t *address = dest + byte_offset;
+    segment = *((uint64_t *)address) | (segment << new_local_bit_offset);
+
+    // Put new segment.
+    int64_t byte_end = GetByteOffset(i + 1, bits_per_entry + 1);
+    int bytes_to_copy = byte_end - byte_offset + 1;
+    memcpy(address, &segment, bytes_to_copy);
+}
+
 static int ExpandHelper(BpArray *array, int new_bits_per_entry) {
-    BpArray new_array;
     int64_t size = array->meta.num_entries;
-    int error = BpArrayInitHelper(&new_array, size, new_bits_per_entry);
-    if (error != 0) {
-        fprintf(stderr,
-                "ExpandHelper: failed to initialize new array, code %d\n",
-                error);
-        return error;
+    int64_t new_stream_length = GetStreamLength(size, new_bits_per_entry);
+    uint8_t *new_stream = (uint8_t *)calloc(new_stream_length, sizeof(uint8_t));
+    if (new_stream == NULL) {
+        fprintf(stderr, "ExpandHelper: failed to calloc new stream\n");
+        return 1;
     }
 
-    // PRAGMA_OMP_PARALLEL_FOR
-    for (int64_t i = 0; i < size; ++i) {
-        uint64_t entry = BpArrayGet(array, i);
-        BpArraySet(&new_array, i, entry);
+    static const int kEntriesPerChunk = 8;
+    int64_t num_chunks = size / kEntriesPerChunk;
+    PRAGMA_OMP_PARALLEL_FOR
+    for (int64_t chunk = 0; chunk < num_chunks; ++chunk) {
+        for (int i = 0; i < kEntriesPerChunk; ++i) {
+            CopyEntry(new_stream, array, chunk * kEntriesPerChunk + i);
+        }
     }
 
-    BpArrayDestroy(array);
-    *array = new_array;
+    for (int64_t i = num_chunks * kEntriesPerChunk; i < size; ++i) {
+        CopyEntry(new_stream, array, i);
+    }
+
+    free(array->stream);
+    array->stream = new_stream;
+    array->meta.bits_per_entry = new_bits_per_entry;
+    array->meta.stream_length = new_stream_length;
     return 0;
 }
 
