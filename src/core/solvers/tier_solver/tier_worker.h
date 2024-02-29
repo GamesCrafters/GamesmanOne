6c36290001e09f76449dff5099d1db7e91bd21a8
Robert Shi <robertyishi@gmail.com>
2024-02-29 02:49:23 -0800

    update version

--- src/core/solvers/tier_solver/tier_worker.h
+++ src/core/solvers/tier_solver/tier_worker.h
@@ -8,8 +8,8 @@
  *         GamesCrafters Research Group, UC Berkeley
  *         Supervised by Dan Garcia <ddgarcia@cs.berkeley.edu>
  * @brief Worker module for the Loopy Tier Solver.
- * @version 1.2.0
- * @date 2024-02-24
+ * @version 1.2.1
+ * @date 2024-02-29
  *
  * @copyright This file is part of GAMESMAN, The Finite, Two-person
  * Perfect-Information Game Generator released under the GPL:
