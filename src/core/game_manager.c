dfedd9bf9c7ac8200242da45d54bab1cd5a6f897
Robert Shi <robertyishi@gmail.com>
2024-02-29 02:53:25 -0800

    update version

--- src/core/game_manager.c
+++ src/core/game_manager.c
@@ -5,9 +5,8 @@
  *         Supervised by Dan Garcia <ddgarcia@cs.berkeley.edu>
  * @brief The Game Manager Module which handles game initialization and
  * finalization.
- *
- * @version 1.1.0
- * @date 2024-01-24
+ * @version 1.1.1
+ * @date 2024-02-29
  *
  * @copyright This file is part of GAMESMAN, The Finite, Two-person
  * Perfect-Information Game Generator released under the GPL:
