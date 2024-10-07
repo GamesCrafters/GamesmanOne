cfe6aaf9c22d52030750dbaf337b90f5ae5c4ba1
Robert Shi <robertyishi@berkeley.edu>
2024-10-06 17:19:28 -0700

    fix potential memory leak and move inside tier worker directory

--- src/core/solvers/tier_solver/tier_worker/reverse_graph.c
+++ src/core/solvers/tier_solver/tier_worker/reverse_graph.c
@@ -9,7 +9,6 @@
  *         GamesCrafters Research Group, UC Berkeley
  *         Supervised by Dan Garcia <ddgarcia@cs.berkeley.edu>
  * @brief Implementation of the ReverseGraph type.
- *
  * @version 1.1.0
  * @date 2023-10-20
  *
