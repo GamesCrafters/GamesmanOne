f70407fa7291aba80fb2ef56a2f390d598838e43
Robert Shi <robertyishi@berkeley.edu>
2026-06-01 21:01:55 -0700

    Add file description for opening_credits.h

--- src/core/opening_credits.h
+++ src/core/opening_credits.h
@@ -1,3 +1,29 @@
+/**
+ * @file opening_credits.h
+ * @author Robert Shi (robertyishi@berkeley.edu)
+ * @author GamesCrafters Research Group, UC Berkeley
+ *         Supervised by Dan Garcia <ddgarcia@cs.berkeley.edu>
+ * @brief Per-frame animation of the opening credits in interactive mode.
+ * @version 1.0.0
+ * @date 2025-04-26
+ *
+ * @copyright This file is part of GAMESMAN, The Finite, Two-person
+ * Perfect-Information Game Generator released under the GPL:
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifdef NDEBUG
 #define THEME "\x1b[38;5;166m"
 #else
