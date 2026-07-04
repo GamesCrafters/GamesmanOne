d6777f5a2da47700b2695c9575d566eba0a30679
Robert Shi <robertyishi@berkeley.edu>
2026-07-03 22:10:43 -0700

    Remove version and date from file headers of unit-tested modules

--- src/libs/xzra/xzra.c
+++ src/libs/xzra/xzra.c
@@ -4,9 +4,6 @@
  * @author GamesCrafters Research Group, UC Berkeley
  *         Supervised by Dan Garcia <ddgarcia@cs.berkeley.edu>
  * @brief XZ utilities with random access.
- * @version 2.0.0
- * @date 2025-06-08
- *
  * @copyright This file is part of GAMESMAN, The Finite, Two-person
  * Perfect-Information Game Generator released under the GPL:
  *
