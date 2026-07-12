89f076bcba1e7c3ad75f5a7b7786be313b5d1291
Robert Shi <robertyishi@berkeley.edu>
2026-07-11 21:54:04 -0700

    Converted concurrency.h into a header only library for better compiler optimizations
    As a result, all other libraries that depend on this module must now explicitly link OpenMP.

--- src/core/concurrency.h
+++ src/core/concurrency.h
@@ -3,11 +3,7 @@
  * @author Robert Shi (robertyishi@berkeley.edu)
  * @author GamesCrafters Research Group, UC Berkeley
  *         Supervised by Dan Garcia <ddgarcia@cs.berkeley.edu>
- * @brief A convenience library for OpenMP pragmas and concurrent data type
- * definitions that work in both single-threaded and multithreaded GamesmanOne
- * builds.
- * @version 1.2.0
- * @date 2025-05-27
+ * @brief Concurrency convenience library (Header-Only).
  *
  * @copyright This file is part of GAMESMAN, The Finite, Two-person
  * Perfect-Information Game Generator released under the GPL:
@@ -31,9 +27,11 @@
 
 #include <stdbool.h>
 #include <stddef.h>
+#include <stdint.h>
 
 #ifdef _OPENMP
 
+#include <omp.h>
 #include <stdatomic.h>
 
 #define PRAGMA(X) _Pragma(#X)
@@ -41,19 +39,48 @@
 
 typedef atomic_bool ConcurrentBool;
 typedef atomic_int ConcurrentInt;
+typedef _Atomic int64_t ConcurrentInt64;
 typedef atomic_size_t ConcurrentSizeType;
+
+/**
+ * @brief Memory order for API of this library.
+ */
+typedef enum {
+    kConcurrencyMemoryOrderRelaxed = memory_order_relaxed,
+    kConcurrencyMemoryOrderConsume = memory_order_consume,
+    kConcurrencyMemoryOrderAcquire = memory_order_acquire,
+    kConcurrencyMemoryOrderRelease = memory_order_release,
+    kConcurrencyMemoryOrderAcqRel = memory_order_acq_rel,
+    kConcurrencyMemoryOrderSeqCst = memory_order_seq_cst,
+} ConcurrencyMemoryOrder;
 
 #else  // _OPENMP not defined.
 
-// The following macros do nothing.
-
+// This macro does nothing when OpenMP is disabled
 #define PRAGMA_OMP(expression)
 
 typedef bool ConcurrentBool;
 typedef int ConcurrentInt;
+typedef int64_t ConcurrentInt64;
 typedef size_t ConcurrentSizeType;
 
+/**
+ * @brief Dummy memory order for API of this library.
+ */
+typedef enum {
+    kConcurrencyMemoryOrderRelaxed,
+    kConcurrencyMemoryOrderConsume,
+    kConcurrencyMemoryOrderAcquire,
+    kConcurrencyMemoryOrderRelease,
+    kConcurrencyMemoryOrderAcqRel,
+    kConcurrencyMemoryOrderSeqCst,
+} ConcurrencyMemoryOrder;
+
 #endif  // _OPENMP
+
+/* ========================================================================= *
+ * ConcurrentBool
+ * ========================================================================= */
 
 /**
  * @brief Initializes the ConcurrentBool variable at \p cb to \p val.
@@ -61,7 +88,13 @@ typedef size_t ConcurrentSizeType;
  * @param cb Pointer to the ConcurrentBool to initialize.
  * @param val Value to initialize to.
  */
-void ConcurrentBoolInit(ConcurrentBool *cb, bool val);
+static inline void ConcurrentBoolInit(ConcurrentBool *cb, bool val) {
+#ifdef _OPENMP
+    atomic_init(cb, val);
+#else
+    *cb = val;
+#endif
+}
 
 /**
  * @brief Returns the current value of the ConcurrentBool variable at \p cb.
@@ -71,7 +104,27 @@ void ConcurrentBoolInit(ConcurrentBool *cb, bool val);
  * @param cb Pointer to the ConcurrentBool variable to load the value from.
  * @return The current value of the ConcurrentBool variable.
  */
-bool ConcurrentBoolLoad(const ConcurrentBool *cb);
+static inline bool ConcurrentBoolLoad(const ConcurrentBool *cb) {
+#ifdef _OPENMP
+    return atomic_load(cb);
+#else
+    return *cb;
+#endif
+}
+
+/**
+ * @brief Returns the current value of the ConcurrentBool variable at \p cb with
+ * explicit memory order.
+ */
+static inline bool ConcurrentBoolLoadExplicit(const ConcurrentBool *cb,
+                                              ConcurrencyMemoryOrder order) {
+#ifdef _OPENMP
+    return atomic_load_explicit(cb, order);
+#else
+    (void)order;
+    return *cb;
+#endif
+}
 
 /**
  * @brief Stores the value \p val into the ConcurrentBool variable at \p cb.
@@ -79,9 +132,33 @@ bool ConcurrentBoolLoad(const ConcurrentBool *cb);
  * order.
  *
  * @param cb Pointer to the ConcurrentBool variable to store the value into.
- * @return The value to store.
+ * @param val The value to store.
  */
-void ConcurrentBoolStore(ConcurrentBool *cb, bool val);
+static inline void ConcurrentBoolStore(ConcurrentBool *cb, bool val) {
+#ifdef _OPENMP
+    atomic_store(cb, val);
+#else
+    *cb = val;
+#endif
+}
+
+/**
+ * @brief Stores the value \p val into the ConcurrentBool variable at \p cb with
+ * explicit memory order.
+ */
+static inline void ConcurrentBoolStoreExplicit(ConcurrentBool *cb, bool val,
+                                               ConcurrencyMemoryOrder order) {
+#ifdef _OPENMP
+    atomic_store_explicit(cb, val, order);
+#else
+    (void)order;
+    *cb = val;
+#endif
+}
+
+/* ========================================================================= *
+ * ConcurrentInt
+ * ========================================================================= */
 
 /**
  * @brief Initializes the ConcurrentInt variable at \p ci to \p val.
@@ -89,7 +166,13 @@ void ConcurrentBoolStore(ConcurrentBool *cb, bool val);
  * @param ci Pointer to the ConcurrentInt to initialize.
  * @param val Value to initialize to.
  */
-void ConcurrentIntInit(ConcurrentInt *ci, int val);
+static inline void ConcurrentIntInit(ConcurrentInt *ci, int val) {
+#ifdef _OPENMP
+    atomic_init(ci, val);
+#else
+    *ci = val;
+#endif
+}
 
 /**
  * @brief Returns the current value of the ConcurrentInt variable at \p ci.
@@ -99,7 +182,27 @@ void ConcurrentIntInit(ConcurrentInt *ci, int val);
  * @param ci Pointer to the ConcurrentInt variable to load the value from.
  * @return The current value of the ConcurrentInt variable.
  */
-int ConcurrentIntLoad(const ConcurrentInt *ci);
+static inline int ConcurrentIntLoad(const ConcurrentInt *ci) {
+#ifdef _OPENMP
+    return atomic_load(ci);
+#else
+    return *ci;
+#endif
+}
+
+/**
+ * @brief Returns the current value of the ConcurrentInt variable at \p ci with
+ * explicit memory order.
+ */
+static inline int ConcurrentIntLoadExplicit(const ConcurrentInt *ci,
+                                            ConcurrencyMemoryOrder order) {
+#ifdef _OPENMP
+    return atomic_load_explicit(ci, order);
+#else
+    (void)order;
+    return *ci;
+#endif
+}
 
 /**
  * @brief Stores the value \p val into the ConcurrentInt variable at \p ci.
@@ -107,9 +210,29 @@ int ConcurrentIntLoad(const ConcurrentInt *ci);
  * order.
  *
  * @param ci Pointer to the ConcurrentInt variable to store the value into.
- * @return The value to store.
+ * @param val The value to store.
  */
-void ConcurrentIntStore(ConcurrentInt *ci, int val);
+static inline void ConcurrentIntStore(ConcurrentInt *ci, int val) {
+#ifdef _OPENMP
+    atomic_store(ci, val);
+#else
+    *ci = val;
+#endif
+}
+
+/**
+ * @brief Stores the value \p val into the ConcurrentInt variable at \p ci with
+ * explicit memory order.
+ */
+static inline void ConcurrentIntStoreExplicit(ConcurrentInt *ci, int val,
+                                              ConcurrencyMemoryOrder order) {
+#ifdef _OPENMP
+    atomic_store_explicit(ci, val, order);
+#else
+    (void)order;
+    *ci = val;
+#endif
+}
 
 /**
  * @brief Replaces the value pointed by \p ci with the maximum of its original
@@ -121,7 +244,99 @@ void ConcurrentIntStore(ConcurrentInt *ci, int val);
  * @return The value pointed by \p ci immediately preceding the effects of this
  * function.
  */
-int ConcurrentIntMax(ConcurrentInt *ci, int val);
+static inline int ConcurrentIntMax(ConcurrentInt *ci, int val) {
+#ifdef _OPENMP
+    int old = atomic_load(ci);
+    while (val > old) {
+        if (atomic_compare_exchange_weak(ci, &old, val)) {
+            break;
+        }
+    }
+    return old;
+#else
+    int ret = *ci;
+    if (val > ret) *ci = val;
+    return ret;
+#endif
+}
+
+/**
+ * @brief Replaces the value pointed by \p ci with the maximum of its original
+ * value and \p val with explicit memory order, and returns the original value.
+ */
+static inline int ConcurrentIntMaxExplicit(ConcurrentInt *ci, int val,
+                                           ConcurrencyMemoryOrder success,
+                                           ConcurrencyMemoryOrder failure) {
+#ifdef _OPENMP
+    int old = atomic_load_explicit(ci, failure);
+    while (val > old) {
+        if (atomic_compare_exchange_weak_explicit(ci, &old, val, success,
+                                                  failure)) {
+            break;
+        }
+    }
+    return old;
+#else
+    (void)success;
+    (void)failure;
+    int ret = *ci;
+    if (val > ret) *ci = val;
+    return ret;
+#endif
+}
+
+/* ========================================================================= *
+ * ConcurrentInt64
+ * ========================================================================= */
+
+static inline void ConcurrentInt64Init(ConcurrentInt64 *ci64, int64_t val) {
+#ifdef _OPENMP
+    atomic_init(ci64, val);
+#else
+    *ci64 = val;
+#endif
+}
+
+static inline int64_t ConcurrentInt64Load(const ConcurrentInt64 *ci64) {
+#ifdef _OPENMP
+    return atomic_load(ci64);
+#else
+    return *ci64;
+#endif
+}
+
+static inline int64_t ConcurrentInt64LoadExplicit(
+    const ConcurrentInt64 *ci64, ConcurrencyMemoryOrder order) {
+#ifdef _OPENMP
+    return atomic_load_explicit(ci64, order);
+#else
+    (void)order;
+    return *ci64;
+#endif
+}
+
+static inline void ConcurrentInt64Store(ConcurrentInt64 *ci64, int64_t val) {
+#ifdef _OPENMP
+    atomic_store(ci64, val);
+#else
+    *ci64 = val;
+#endif
+}
+
+static inline void ConcurrentInt64StoreExplicit(ConcurrentInt64 *ci64,
+                                                int64_t val,
+                                                ConcurrencyMemoryOrder order) {
+#ifdef _OPENMP
+    atomic_store_explicit(ci64, val, order);
+#else
+    (void)order;
+    *ci64 = val;
+#endif
+}
+
+/* ========================================================================= *
+ * ConcurrentSizeType
+ * ========================================================================= */
 
 /**
  * @brief Initializes the ConcurrentSizeType variable at \p cs to \p val.
@@ -129,7 +344,13 @@ int ConcurrentIntMax(ConcurrentInt *ci, int val);
  * @param cs Pointer to the ConcurrentSizeType to initialize.
  * @param val Value to initialize to.
  */
-void ConcurrentSizeTypeInit(ConcurrentSizeType *cs, size_t val);
+static inline void ConcurrentSizeTypeInit(ConcurrentSizeType *cs, size_t val) {
+#ifdef _OPENMP
+    atomic_init(cs, val);
+#else
+    *cs = val;
+#endif  // _OPENMP
+}
 
 /**
  * @brief Returns the current value of the ConcurrentSizeType variable at \p cs.
@@ -139,7 +360,27 @@ void ConcurrentSizeTypeInit(ConcurrentSizeType *cs, size_t val);
  * @param cs Pointer to the ConcurrentSizeType variable to load the value from.
  * @return The current value of the ConcurrentSizeType variable.
  */
-size_t ConcurrentSizeTypeLoad(const ConcurrentSizeType *cs);
+static inline size_t ConcurrentSizeTypeLoad(const ConcurrentSizeType *cs) {
+#ifdef _OPENMP
+    return atomic_load(cs);
+#else
+    return *cs;
+#endif  // _OPENMP
+}
+
+/**
+ * @brief Returns the current value of the ConcurrentSizeType variable at \p cs
+ * with explicit memory order.
+ */
+static inline size_t ConcurrentSizeTypeLoadExplicit(
+    const ConcurrentSizeType *cs, ConcurrencyMemoryOrder order) {
+#ifdef _OPENMP
+    return atomic_load_explicit(cs, order);
+#else
+    (void)order;
+    return *cs;
+#endif  // _OPENMP
+}
 
 /**
  * @brief Subtracts \p val from the ConcurrentSizeType variable at \p cs if and
@@ -152,8 +393,47 @@ size_t ConcurrentSizeTypeLoad(const ConcurrentSizeType *cs);
  * @return \c true if the subtraction is successfully completed, or
  * @return \c false if the current value of \p cs is smaller than \p val.
  */
-bool ConcurrentSizeTypeSubtractIfGreaterEqual(ConcurrentSizeType *cs,
-                                              size_t val);
+static inline bool ConcurrentSizeTypeSubtractIfGreaterEqual(
+    ConcurrentSizeType *cs, size_t val) {
+#ifdef _OPENMP
+    size_t cur_size = atomic_load(cs);
+    while (cur_size >= val) {
+        bool success =
+            atomic_compare_exchange_weak(cs, &cur_size, cur_size - val);
+        if (success) return true;
+    }
+    return false;
+#else
+    if (*cs < val) return false;
+    *cs -= val;
+    return true;
+#endif  // _OPENMP
+}
+
+/**
+ * @brief Subtracts \p val from the ConcurrentSizeType variable at \p cs if and
+ * only if its current value is greater than or equal to \p val, with explicit
+ * memory order.
+ */
+static inline bool ConcurrentSizeTypeSubtractIfGreaterEqualExplicit(
+    ConcurrentSizeType *cs, size_t val, ConcurrencyMemoryOrder success,
+    ConcurrencyMemoryOrder failure) {
+#ifdef _OPENMP
+    size_t cur_size = atomic_load_explicit(cs, failure);
+    while (cur_size >= val) {
+        bool op_success = atomic_compare_exchange_weak_explicit(
+            cs, &cur_size, cur_size - val, success, failure);
+        if (op_success) return true;
+    }
+    return false;
+#else
+    (void)success;
+    (void)failure;
+    if (*cs < val) return false;
+    *cs -= val;
+    return true;
+#endif  // _OPENMP
+}
 
 /**
  * @brief Adds \p val to the ConcurrentSizeType variable at \p cs and returns
@@ -164,7 +444,31 @@ bool ConcurrentSizeTypeSubtractIfGreaterEqual(ConcurrentSizeType *cs,
  * @param val Value to add.
  * @return The original value in \p cs before the addition.
  */
-size_t ConcurrentSizeTypeAdd(ConcurrentSizeType *cs, size_t val);
+static inline size_t ConcurrentSizeTypeAdd(ConcurrentSizeType *cs, size_t val) {
+#ifdef _OPENMP
+    return atomic_fetch_add(cs, val);
+#else
+    size_t ret = *cs;
+    *cs += val;
+    return ret;
+#endif  // _OPENMP
+}
+
+/**
+ * @brief Adds \p val to the ConcurrentSizeType variable at \p cs and returns
+ * its original value, using explicit memory order.
+ */
+static inline size_t ConcurrentSizeTypeAddExplicit(
+    ConcurrentSizeType *cs, size_t val, ConcurrencyMemoryOrder order) {
+#ifdef _OPENMP
+    return atomic_fetch_add_explicit(cs, val, order);
+#else
+    (void)order;
+    size_t ret = *cs;
+    *cs += val;
+    return ret;
+#endif  // _OPENMP
+}
 
 /**
  * @brief Subtracts \p val to the ConcurrentSizeType variable at \p cs and
@@ -176,7 +480,36 @@ size_t ConcurrentSizeTypeAdd(ConcurrentSizeType *cs, size_t val);
  * @param val Value to add.
  * @return The original value in \p cs before the subtraction.
  */
-size_t ConcurrentSizeTypeSubtract(ConcurrentSizeType *cs, size_t val);
+static inline size_t ConcurrentSizeTypeSubtract(ConcurrentSizeType *cs,
+                                                size_t val) {
+#ifdef _OPENMP
+    return atomic_fetch_sub(cs, val);
+#else
+    size_t ret = *cs;
+    *cs -= val;
+    return ret;
+#endif  // _OPENMP
+}
+
+/**
+ * @brief Subtracts \p val to the ConcurrentSizeType variable at \p cs and
+ * returns its original value, using explicit memory order.
+ */
+static inline size_t ConcurrentSizeTypeSubtractExplicit(
+    ConcurrentSizeType *cs, size_t val, ConcurrencyMemoryOrder order) {
+#ifdef _OPENMP
+    return atomic_fetch_sub_explicit(cs, val, order);
+#else
+    (void)order;
+    size_t ret = *cs;
+    *cs -= val;
+    return ret;
+#endif  // _OPENMP
+}
+
+/* ========================================================================= *
+ * OpenMP Thread Utilities
+ * ========================================================================= */
 
 /**
  * @brief Returns the number of OpenMP threads available. Returns 1 if
@@ -184,7 +517,13 @@ size_t ConcurrentSizeTypeSubtract(ConcurrentSizeType *cs, size_t val);
  *
  * @return Number of OpenMP threads available.
  */
-int ConcurrencyGetOmpNumThreads(void);
+static inline int ConcurrencyGetOmpNumThreads(void) {
+#ifdef _OPENMP
+    return omp_get_max_threads();
+#else   // _OPENMP not defined.
+    return 1;
+#endif  // _OPENMP
+}
 
 /**
  * @brief Returns the caller's OpenMP thread ID. Returns 0 if OpenMP is
@@ -192,6 +531,12 @@ int ConcurrencyGetOmpNumThreads(void);
  *
  * @return The caller's OpenMP thread ID.
  */
-int ConcurrencyGetOmpThreadId(void);
+static inline int ConcurrencyGetOmpThreadId(void) {
+#ifdef _OPENMP
+    return omp_get_thread_num();
+#else   // _OPENMP not defined, thread 0 is the only available thread.
+    return 0;
+#endif  // _OPENMP
+}
 
 #endif  // GAMESMANONE_CORE_CONCURRENCY_H_
