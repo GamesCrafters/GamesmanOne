ac4dbbb6eb23f7fbf78d736d2efc9fcbb20c4b68
Robert Shi <robertyishi@berkeley.edu>
2026-08-04 22:57:11 -0700

    Add Doxygen dependency

--- gamesman_setup.sh
+++ gamesman_setup.sh
@@ -84,7 +84,7 @@ install_rhel() {
 # Function to install dependencies on MacOS
 install_macos() {
     xcode-select --install 2>/dev/null # Suppress error if already installed
-    brew install cmake zlib llvm libomp jq ninja open-mpi || return 1
+    brew install cmake zlib llvm libomp jq ninja open-mpi doxygen dot || return 1
     brew install ccache 2>/dev/null || echo "Note: ccache could not be installed. Continuing without it..."
     brew install include-what-you-use 2>/dev/null || echo "Note: include-what-you-use could not be installed. Continuing without it..."
     brew install lcov 2>/dev/null || echo "Note: lcov could not be installed. Continuing without it..."
