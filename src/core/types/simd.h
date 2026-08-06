894cdc53c91e3f37c4a4b3ae7f7922595bdc2fbd
Robert Shi <robertyishi@berkeley.edu>
2026-08-05 21:20:12 -0700

    BMI1 detection and compile-time instruction switching

--- src/core/types/simd.h
+++ src/core/types/simd.h
@@ -1,3 +1,27 @@
+/**
+ * @file simd.h
+ * @author Robert Shi (robertyishi@berkeley.edu)
+ * @author GamesCrafters Research Group, UC Berkeley
+ *         Supervised by Dan Garcia <ddgarcia@cs.berkeley.edu>
+ * @brief SIMD helper library.
+ *
+ * @copyright This file is part of GAMESMAN, The Finite, Two-person
+ * Perfect-Information Game Generator released under the GPL:
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef GAMESMANONE_CORE_TYPES_SIMD_H_
 #define GAMESMANONE_CORE_TYPES_SIMD_H_
 
@@ -9,9 +33,9 @@ typedef uint64_t U64x2 __attribute__((vector_size(16)));
 
 #include <stdint.h>
 
-#ifdef GAMESMAN_HAS_BMI2
+#ifdef GAMESMAN_HAS_BMI1
 #include <immintrin.h>
-#endif
+#endif  // GAMESMAN_HAS_BMI1
 
 static inline uint64_t PextU64(uint64_t val, uint64_t mask) {
 #ifdef GAMESMAN_HAS_BMI2
@@ -43,8 +67,20 @@ static inline uint64_t PdepU64(uint64_t val, uint64_t mask) {
 #endif
 }
 
-static inline uint64_t BlsrU64(uint64_t x) { return x & (x - 1); }
+static inline uint64_t BlsrU64(uint64_t x) {
+#ifdef GAMESMAN_HAS_BMI1
+    return _blsr_u64(x);
+#else   // No BMI1
+    return x & (x - 1);
+#endif  // GAMESMAN_HAS_BMI1
+}
 
-static inline uint64_t BlsiU64(uint64_t x) { return x & -x; }
+static inline uint64_t BlsiU64(uint64_t x) {
+#ifdef GAMESMAN_HAS_BMI1
+    return _blsi_u64(x);
+#else   // No BMI1
+    return x & -x;
+#endif  // GAMESMAN_HAS_BMI1
+}
 
 #endif  // GAMESMANONE_CORE_TYPES_SIMD_H_
